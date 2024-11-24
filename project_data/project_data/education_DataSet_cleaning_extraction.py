128c743ceb29804d5d896d350efc8d885bf69b7a
Naman Singh <namansingh2623@gmail.com>
2024-11-23 23:22:39 -0500

    Added Median Annual Wage Change Graph

--- project_data/project_data/education_DataSet_cleaning_extraction.py
+++ project_data/project_data/education_DataSet_cleaning_extraction.py
@@ -20,3 +20,5 @@ def load_table_to_dataframe(file_path, sheet_name, header_row, num_rows):
     except Exception as e:
         print(f"Error while loading the table: {e}")
         return None
+    
+
